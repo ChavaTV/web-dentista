aff4ab2941ffa077cad82a5a4d7740df752064b5
Salvador Torres <126022222+ChavaTV@users.noreply.github.com>
2025-09-11 11:31:42 -0600

    conexion con firebase

--- consultorio-dental-frontend/src/App.jsx
+++ consultorio-dental-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard_card from './pages/Dashboard_card';
 import Pacientes from './pages/Pacientes';
 import Calendario from './pages/Calendario';
 import Agendar from './pages/Agendar';
+import Login from './pages/login';
 // import Dentistas from './pages/Dentistas';
 // import Facturacion from './pages/Facturacion';
 
@@ -11,31 +12,19 @@ function App() {
   return (
     <Router basename="/web-dentista">
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/" element={<Dashboard />} /> */}
         <Route path="card" element={<Dashboard_card />} />
         <Route path="/pacientes" element={<Pacientes />} />
         <Route path="/Calendario" element={<Calendario />} />
         <Route path="/agendar" element={<Agendar />} />
-        {/*<Route path="/facturacion" element={<Facturacion />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   return (
-//     <div className="flex justify-center items-center h-screen bg-blue-500">
-//       <h1 className="text-white text-4xl">¡Tailwind está funcionando!</h1>
-//     </div>
-//   )
-// }
-
-// export default App;
-
